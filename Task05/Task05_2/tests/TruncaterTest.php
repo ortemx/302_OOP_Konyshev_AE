48a34667451e828be84b6fc32c87158b4d3de937
ortemx <printerq@yandex.ru>
2023-04-08 22:31:51 +0300

    Update TruncaterTest.php

--- Task05/Task05_2/tests/TruncaterTest.php
+++ Task05/Task05_2/tests/TruncaterTest.php
@@ -11,14 +11,26 @@ class TruncaterTest extends TestCase
     {
         $defaultTruncater = new Truncater();
         $this->assertSame("Конышев Артем Евгеньевич", $defaultTruncater->truncate("Конышев Артем Евгеньевич"));
-        $this->assertSame("Конышев Ар...", $defaultTruncater->truncate("Конышев Артем Евгеньевич", ['length' => 10]));
-        $this->assertSame("Конышев Артем ...", $defaultTruncater->truncate("Конышев Артем Евгеньевич", ['length' => -10]));
-        $this->assertSame("Конышев Ар*", $defaultTruncater->truncate("Конышев Артем Евгеньевич", ['length' => 10, 'separator' => '*']));
+        $this->assertSame("Конышев Ар...", $defaultTruncater->truncate(
+            "Конышев Артем Евгеньевич",
+            ['length' => 10]
+        ));
+        $this->assertSame("Конышев Артем ...", $defaultTruncater->truncate(
+            "Конышев Артем Евгеньевич",
+            ['length' => -10]
+        ));
+        $this->assertSame("Конышев Ар*", $defaultTruncater->truncate(
+            "Конышев Артем Евгеньевич",
+            ['length' => 10, 'separator' => '*']
+        ));
         $this->assertSame("Конышев Артем Евгеньевич", $defaultTruncater->truncate("Конышев Артем Евгеньевич"));
 
         $overriddenTruncater1 = new Truncater(['length' => 14]);
         $this->assertSame("Конышев Артем ...", $overriddenTruncater1->truncate("Конышев Артем Евгеньевич"));
-        $this->assertSame("Конышев Артем \\", $overriddenTruncater1->truncate("Конышев Артем Евгеньевич", ['separator' => '\\']));
+        $this->assertSame("Конышев Артем \\", $overriddenTruncater1->truncate(
+            "Конышев Артем Евгеньевич",
+            ['separator' => '\\']
+        ));
 
         $overriddenTruncater2 = new Truncater(['length' => 14, 'separator' => '***']);
         $this->assertSame("Конышев Артем ***", $overriddenTruncater2->truncate("Конышев Артем Евгеньевич"));
