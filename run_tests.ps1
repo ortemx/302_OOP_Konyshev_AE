125eeb6add0a53139da5313d45d96b05bd4aec72
ortemx <printerq@yandex.ru>
2023-05-08 02:15:07 +0300

    Update run_tests.ps1

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,15 +1,27 @@
-Set-Location Task06/Task06_1/
-vendor/bin/phpunit --color ./tests
+# Set-Location Task06/Task06_1/
+# vendor/bin/phpunit --color ./tests
+# Set-Location ../..
+
+# Set-Location Task06/Task06_2/
+# vendor/bin/phpunit --color ./tests
+# Set-Location ../..
+
+# Set-Location Task05/Task05_1/
+# vendor/bin/phpunit --color ./tests
+# Set-Location ../..
+
+# Set-Location Task05/Task05_2/
+# vendor/bin/phpunit --color ./tests
+# Set-Location ../..
+
+Set-Location Task07/Task07_1/
+npm run test
 Set-Location ../..
 
-Set-Location Task06/Task06_2/
-vendor/bin/phpunit --color ./tests
+Set-Location Task07/Task07_2/
+npm run test
 Set-Location ../..
 
-Set-Location Task05/Task05_1/
-vendor/bin/phpunit --color ./tests
-Set-Location ../..
-
-Set-Location Task05/Task05_2/
-vendor/bin/phpunit --color ./tests
+Set-Location Task07/Task07_3/
+npm run test
 Set-Location ../..
